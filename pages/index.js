d386616084b6680d08e2e49942d4debc7d5048bf
Agmar Putra <agmar.putra@spesolution.net>
2021-10-19 14:13:56 +0700

    menambahkan dynamic load di pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-import Dashboard from '../modules/Dashboard'
+import dynamic from "next/dynamic"
+
+const Dashboard = dynamic(() => import('../modules/Dashboard'), {
+  loading: () => <div>Loading Content ...</div>
+})
 
 export default function Home() {
   return (
