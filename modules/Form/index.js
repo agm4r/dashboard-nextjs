5480a8f132f08e6d31ba11244e8cb06359ed2b32
Agmar Putra <agmar.putra@spesolution.net>
2021-10-26 12:45:57 +0700

    add new fitur in input hobbies

--- modules/Form/index.js
+++ modules/Form/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useRouter } from 'next/router';
 import { 
   Typography, 
   TextField, 
@@ -8,7 +9,11 @@ import {
   Radio, 
   RadioGroup, 
   Autocomplete,
-  Grid
+  Grid,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions
 } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
@@ -20,8 +25,7 @@ import moment from 'moment'
 import { useFormik } from 'formik';
 import * as yup from 'yup';
 import FormHelperText from '@mui/material/FormHelperText'
-
-const phoneRegExp = '/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/'
+import useFetch from '../../components/useFetch'
 
 const validationSchema = yup.object({
   name: yup
@@ -68,6 +72,35 @@ const useStyles = makeStyles({
 })
 
 const Form = () => {
+  const urlHobbies = 'http://localhost:3000/hobbies'
+  const urlMovies = 'http://localhost:8000/movies'
+  const { data: listHobbies } = useFetch(urlHobbies)
+  const { data: top100Films } = useFetch(urlMovies)
+
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState('')
+
+  const router = useRouter()
+
+  const handleSubmit = (e) => {
+		e.preventDefault()
+		const hobbies = {title}
+
+		fetch(urlHobbies, {
+			method:'POST',
+			headers: { 'Content-Type': "application/json" },
+			body: JSON.stringify(hobbies)
+		}).then(() => {
+			console.log('new hobbies added')
+      handleClose()
+      router.push('/form')
+		})
+	}
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const classes = useStyles()
 
   const [value, setValue] = useState({
@@ -103,8 +136,8 @@ const Form = () => {
       gender: value.gender,
       birth: value.birth,
       education: value.education,
-      hobbies: [],
-      movies: []
+      hobbies: value.hobbies,
+      movies: null,
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -182,20 +215,6 @@ return (
             variant="standard" 
           />
         </Grid>
-        {/* <Grid item xs={12}>
-          <TextField 
-            id="phone"
-            name="phone"
-            label="Phone"
-            type="text"
-            variant="standard"
-            value={formik.values.phone}
-            onChange={formik.handleChange}
-            error={formik.touched.phone && Boolean(formik.errors.phone)}
-            // error={console.log(formik.errors)}
-            helpertext={formik.touched.phone && formik.errors.phone}
-          />
-        </Grid> */}
         <Grid item xs={12}>
           <FormLabel>Gender</FormLabel>
           <RadioGroup 
@@ -246,15 +265,23 @@ return (
         </Grid>
         <Grid item xs={12}>
           <Autocomplete
-            multiple
+            multiple={true}
             id="hobbies"
             name ="hobbies"
             label ="hobbies"
             value={formik.values.hobbies || []}
-            options={hobbies}
-            onChange={(event, value) => {
-              formik.setFieldValue('hobbies', value)
-              console.log(formik.values.hobbies)}
+            options={listHobbies}
+            onChange={(event, value) => {      
+              formik.setFieldValue('hobbies', value)  
+              value.map(hobbies => {
+                if (hobbies.id == 1) {
+                  console.log('tambah data')
+                  value.splice(value.indexOf(hobbies.id), 1)
+                  setOpen(true)
+                  console.log(value)
+                }
+              })
+            }
             }
             getOptionLabel={(option) => option.title}
             renderInput={(params) => (
@@ -266,7 +293,26 @@ return (
               />
             )}
           />
-          <FormHelperText error={formik.touched.hobbies && Boolean(formik.errors.hobbies)}>{formik.touched.hobbies && formik.errors.hobbies}</FormHelperText>   
+          <FormHelperText error={formik.touched.hobbies && Boolean(formik.errors.hobbies)}>{formik.touched.hobbies && formik.errors.hobbies}</FormHelperText> 
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Add new hobby</DialogTitle>
+            <form onSubmit={handleSubmit}>
+              <DialogContent>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="newHobby"
+                  fullWidth
+                  variant="standard"
+                  onChange={ (e) => setTitle(e.target.value) }
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button type="submit">Add</Button>
+              </DialogActions>
+            </form>
+          </Dialog>
         </Grid>
         <Grid item xs={12}>
           <Autocomplete
@@ -290,7 +336,7 @@ return (
               />
             )}
           />
-          <FormHelperText error={formik.touched.movies && Boolean(formik.errors.movies)}>{formik.touched.movies && formik.errors.movies}</FormHelperText>   
+          <FormHelperText error={formik.touched.movies && Boolean(formik.errors.movies)}>{formik.touched.movies && formik.errors.movies}</FormHelperText>  
         </Grid>
         <Grid item xs={12}>
         <Button
@@ -319,140 +365,141 @@ const pendidikan = [
   { id: 6, label: 'S3', },
 ]
 
-const hobbies = [
-  {id: 1, title: "WATCHING MOVIE" },
-  {id: 2, title: "SPORTS" },
-  {id: 3, title: "MUSIC" },
-  {id: 4, title: "DRAWING" },
-  {id: 5, title: "ART" },
-  {id: 6, title: "GAME" },
-  {id: 7, title: "E SPORTS" },
-];
+// const hobbies = [
+//   {id: 1, title: "WATCHING MOVIE" },
+//   {id: 2, title: "SPORTS" },
+//   {id: 3, title: "MUSIC" },
+//   {id: 4, title: "DRAWING" },
+//   {id: 5, title: "ART" },
+//   {id: 6, title: "GAME" },
+//   {id: 7, title: "E SPORTS" },
+// ];
 
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-  { title: 'Casablanca', year: 1942 },
-  { title: 'City Lights', year: 1931 },
-  { title: 'Psycho', year: 1960 },
-  { title: 'The Green Mile', year: 1999 },
-  { title: 'The Intouchables', year: 2011 },
-  { title: 'Modern Times', year: 1936 },
-  { title: 'Raiders of the Lost Ark', year: 1981 },
-  { title: 'Rear Window', year: 1954 },
-  { title: 'The Pianist', year: 2002 },
-  { title: 'The Departed', year: 2006 },
-  { title: 'Terminator 2: Judgment Day', year: 1991 },
-  { title: 'Back to the Future', year: 1985 },
-  { title: 'Whiplash', year: 2014 },
-  { title: 'Gladiator', year: 2000 },
-  { title: 'Memento', year: 2000 },
-  { title: 'The Prestige', year: 2006 },
-  { title: 'The Lion King', year: 1994 },
-  { title: 'Apocalypse Now', year: 1979 },
-  { title: 'Alien', year: 1979 },
-  { title: 'Sunset Boulevard', year: 1950 },
-  {
-    title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    year: 1964,
-  },
-  { title: 'The Great Dictator', year: 1940 },
-  { title: 'Cinema Paradiso', year: 1988 },
-  { title: 'The Lives of Others', year: 2006 },
-  { title: 'Grave of the Fireflies', year: 1988 },
-  { title: 'Paths of Glory', year: 1957 },
-  { title: 'Django Unchained', year: 2012 },
-  { title: 'The Shining', year: 1980 },
-  { title: 'WALL·E', year: 2008 },
-  { title: 'American Beauty', year: 1999 },
-  { title: 'The Dark Knight Rises', year: 2012 },
-  { title: 'Princess Mononoke', year: 1997 },
-  { title: 'Aliens', year: 1986 },
-  { title: 'Oldboy', year: 2003 },
-  { title: 'Once Upon a Time in America', year: 1984 },
-  { title: 'Witness for the Prosecution', year: 1957 },
-  { title: 'Das Boot', year: 1981 },
-  { title: 'Citizen Kane', year: 1941 },
-  { title: 'North by Northwest', year: 1959 },
-  { title: 'Vertigo', year: 1958 },
-  {
-    title: 'Star Wars: Episode VI - Return of the Jedi',
-    year: 1983,
-  },
-  { title: 'Reservoir Dogs', year: 1992 },
-  { title: 'Braveheart', year: 1995 },
-  { title: 'M', year: 1931 },
-  { title: 'Requiem for a Dream', year: 2000 },
-  { title: 'Amélie', year: 2001 },
-  { title: 'A Clockwork Orange', year: 1971 },
-  { title: 'Like Stars on Earth', year: 2007 },
-  { title: 'Taxi Driver', year: 1976 },
-  { title: 'Lawrence of Arabia', year: 1962 },
-  { title: 'Double Indemnity', year: 1944 },
-  {
-    title: 'Eternal Sunshine of the Spotless Mind',
-    year: 2004,
-  },
-  { title: 'Amadeus', year: 1984 },
-  { title: 'To Kill a Mockingbird', year: 1962 },
-  { title: 'Toy Story 3', year: 2010 },
-  { title: 'Logan', year: 2017 },
-  { title: 'Full Metal Jacket', year: 1987 },
-  { title: 'Dangal', year: 2016 },
-  { title: 'The Sting', year: 1973 },
-  { title: '2001: A Space Odyssey', year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: 'Toy Story', year: 1995 },
-  { title: 'Bicycle Thieves', year: 1948 },
-  { title: 'The Kid', year: 1921 },
-  { title: 'Inglourious Basterds', year: 2009 },
-  { title: 'Snatch', year: 2000 },
-  { title: '3 Idiots', year: 2009 },
-  { title: 'Monty Python and the Holy Grail', year: 1975 },
-];
+// const top100Films = [
+//   { title: 'The Shawshank Redemption', year: 1994 },
+//   { title: 'The Godfather', year: 1972 },
+//   { title: 'The Godfather: Part II', year: 1974 },
+//   { title: 'The Dark Knight', year: 2008 },
+//   { title: '12 Angry Men', year: 1957 },
+//   { title: "Schindler's List", year: 1993 },
+//   { title: 'Pulp Fiction', year: 1994 },
+//   {
+//     title: 'The Lord of the Rings: The Return of the King',
+//     year: 2003,
+//   },
+//   { title: 'The Good, the Bad and the Ugly', year: 1966 },
+//   { title: 'Fight Club', year: 1999 },
+//   {
+//     title: 'The Lord of the Rings: The Fellowship of the Ring',
+//     year: 2001,
+//   },
+//   {
+//     title: 'Star Wars: Episode V - The Empire Strikes Back',
+//     year: 1980,
+//   },
+//   { title: 'Forrest Gump', year: 1994 },
+//   { title: 'Inception', year: 2010 },
+//   {
+//     title: 'The Lord of the Rings: The Two Towers',
+//     year: 2002,
+//   },
+//   { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+//   { title: 'Goodfellas', year: 1990 },
+//   { title: 'The Matrix', year: 1999 },
+//   { title: 'Seven Samurai', year: 1954 },
+//   {
+//     title: 'Star Wars: Episode IV - A New Hope',
+//     year: 1977,
+//   },
+//   { title: 'City of God', year: 2002 },
+//   { title: 'Se7en', year: 1995 },
+//   { title: 'The Silence of the Lambs', year: 1991 },
+//   { title: "It's a Wonderful Life", year: 1946 },
+//   { title: 'Life Is Beautiful', year: 1997 },
+//   { title: 'The Usual Suspects', year: 1995 },
+//   { title: 'Léon: The Professional', year: 1994 },
+//   { title: 'Spirited Away', year: 2001 },
+//   { title: 'Saving Private Ryan', year: 1998 },
+//   { title: 'Once Upon a Time in the West', year: 1968 },
+//   { title: 'American History X', year: 1998 },
+//   { title: 'Interstellar', year: 2014 },
+//   { title: 'Casablanca', year: 1942 },
+//   { title: 'City Lights', year: 1931 },
+//   { title: 'Psycho', year: 1960 },
+//   { title: 'The Green Mile', year: 1999 },
+//   { title: 'The Intouchables', year: 2011 },
+//   { title: 'Modern Times', year: 1936 },
+//   { title: 'Raiders of the Lost Ark', year: 1981 },
+//   { title: 'Rear Window', year: 1954 },
+//   { title: 'The Pianist', year: 2002 },
+//   { title: 'The Departed', year: 2006 },
+//   { title: 'Terminator 2: Judgment Day', year: 1991 },
+//   { title: 'Back to the Future', year: 1985 },
+//   { title: 'Whiplash', year: 2014 },
+//   { title: 'Gladiator', year: 2000 },
+//   { title: 'Memento', year: 2000 },
+//   { title: 'The Prestige', year: 2006 },
+//   { title: 'The Lion King', year: 1994 },
+//   { title: 'Apocalypse Now', year: 1979 },
+//   { title: 'Alien', year: 1979 },
+//   { title: 'Sunset Boulevard', year: 1950 },
+//   {
+//     title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+//     year: 1964,
+//   },
+//   { title: 'The Great Dictator', year: 1940 },
+//   { title: 'Cinema Paradiso', year: 1988 },
+//   { title: 'The Lives of Others', year: 2006 },
+//   { title: 'Grave of the Fireflies', year: 1988 },
+//   { title: 'Paths of Glory', year: 1957 },
+//   { title: 'Django Unchained', year: 2012 },
+//   { title: 'The Shining', year: 1980 },
+//   { title: 'WALL·E', year: 2008 },
+//   { title: 'American Beauty', year: 1999 },
+//   { title: 'The Dark Knight Rises', year: 2012 },
+//   { title: 'Princess Mononoke', year: 1997 },
+//   { title: 'Aliens', year: 1986 },
+//   { title: 'Oldboy', year: 2003 },
+//   { title: 'Once Upon a Time in America', year: 1984 },
+//   { title: 'Witness for the Prosecution', year: 1957 },
+//   { title: 'Das Boot', year: 1981 },
+//   { title: 'Citizen Kane', year: 1941 },
+//   { title: 'North by Northwest', year: 1959 },
+//   { title: 'Vertigo', year: 1958 },
+//   {
+//     title: 'Star Wars: Episode VI - Return of the Jedi',
+//     year: 1983,
+//   },
+//   { title: 'Reservoir Dogs', year: 1992 },
+//   { title: 'Braveheart', year: 1995 },
+//   { title: 'M', year: 1931 },
+//   { title: 'Requiem for a Dream', year: 2000 },
+//   { title: 'Amélie', year: 2001 },
+//   { title: 'A Clockwork Orange', year: 1971 },
+//   { title: 'Like Stars on Earth', year: 2007 },
+//   { title: 'Taxi Driver', year: 1976 },
+//   { title: 'Lawrence of Arabia', year: 1962 },
+//   { title: 'Double Indemnity', year: 1944 },
+//   {
+//     title: 'Eternal Sunshine of the Spotless Mind',
+//     year: 2004,
+//   },
+//   { title: 'Amadeus', year: 1984 },
+//   { title: 'To Kill a Mockingbird', year: 1962 },
+//   { title: 'Toy Story 3', year: 2010 },
+//   { title: 'Logan', year: 2017 },
+//   { title: 'Full Metal Jacket', year: 1987 },
+//   { title: 'Dangal', year: 2016 },
+//   { title: 'The Sting', year: 1973 },
+//   { title: '2001: A Space Odyssey', year: 1968 },
+//   { title: "Singin' in the Rain", year: 1952 },
+//   { title: 'Toy Story', year: 1995 },
+//   { title: 'Bicycle Thieves', year: 1948 },
+//   { title: 'The Kid', year: 1921 },
+//   { title: 'Inglourious Basterds', year: 2009 },
+//   { title: 'Snatch', year: 2000 },
+//   { title: '3 Idiots', year: 2009 },
+//   { title: 'Monty Python and the Holy Grail', year: 1975 },
+// ];
+
